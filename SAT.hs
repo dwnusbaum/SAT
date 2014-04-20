6345098d79d8ae529eea1c4083a210e3415e139b
Devin Nusbaum <d.w.nusbaum@gmail.com>
2014-04-19 22:20:08 -0400

    Bugfixes. Now implemented through v.4 in the paper

--- SAT.hs
+++ SAT.hs
@@ -91,7 +91,7 @@ prefixBeforeLastDecision ls = takeWhile ((/= l) . fst) ls
   where l = lastDecision ls
 
 vars :: Formula -> [Literal]
-vars = S.toList . S.unions . map S.fromList
+vars = S.toList . S.unions . map (S.fromList . map abs)
 
 --Conflict resolution
 
@@ -113,12 +113,13 @@ removeLiteral s@(S _ ls c@(C cH cP _ cN) _) l =
            then s { conflict = c { cMap = cH', cNum = cN - 1 } }
            else s { conflict = c { cMap = cH', cPartial = S.toList . S.delete l . S.fromList $ cP } }
 {-
-buildConflict :: Conflict -> Clause
-buildConflict (C _ cP cL _) = cP ++ [cL]
+buildC :: Conflict -> Conflict
+buildC c@(C _ cP cL _) = c { cFull = cP ++ [negate cL] }
 -}
 
 applyConflict :: State -> State
-applyConflict s@(S f ls _ _) = foldl addLiteral s $ getConflictClause ls f
+applyConflict s@(S f ls _ _) = findLastAssertedLiteral $ foldl addLiteral s $ trace ("Conflict: " ++ show conflictClause) conflictClause
+  where conflictClause = getConflictClause ls f
 
 explainEmpty :: State -> State
 explainEmpty s@(S _ _ (C _ cP cL _) _)
@@ -126,9 +127,8 @@ explainEmpty s@(S _ _ (C _ cP cL _) _)
   | otherwise      = explainEmpty $ explain s cL
 
 explainUIP :: State -> State
-explainUIP s@(S _ _ (C _ cP cL _) _)
-  | null $ cL : cP = s
-  | otherwise      = if not $ isUIP s then explainUIP $ explain s cL else s
+explainUIP s = if not $ isUIP s then explainUIP $ explain s cL else s
+  where cL = cLast $ conflict s
 
 explain :: State -> Literal -> State
 explain s l = trace ("Explained " ++ show l ++ " by " ++ show (getConflictReason s l)) $
@@ -142,13 +142,13 @@ findLastAssertedLiteral s@(S _ ls c _) = s { conflict = c { cLast = lastAsserted
   where lastAsserted = head [ l | l <- reverse (map fst ls), M.findWithDefault False (-l) $ cMap c ]
 
 getConflictReason :: State -> Literal -> Clause
-getConflictReason s l = M.findWithDefault (error "No reason for literal propogation") l (reasons s)
+getConflictReason s l = M.findWithDefault (error $ "No reason for literal propogation: " ++ show l) l (reasons s)
 
 setConflictReason :: State -> Literal -> Clause -> State
 setConflictReason s l c = s { reasons = M.insert l c $ reasons s }
 
 isUIP :: State -> Bool
-isUIP (S _ _ (C _ _ _ cN) _) = cN == 1
+isUIP = (== 1) . cNum . conflict
 
 lastAssertedLiteral :: Clause -> LiteralTrail -> Literal
 lastAssertedLiteral c = last . filter (`elem` c) . map fst
@@ -156,14 +156,16 @@ lastAssertedLiteral c = last . filter (`elem` c) . map fst
 learn :: State -> State
 learn s = trace ("Learned " ++ show c') $ s { formula = formula s ++ [c'] }
   where c  = conflict s
-        c' = cPartial c ++ [cLast c]
+        c' = cPartial c ++ [negate $ cLast c]
 
 --Backjumping
 
 backjump :: State -> State
-backjump s@(S _ ls (C _ cP cL _) _) = setConflictReason (assertLiteral s' (-cL) False) (-cL) $ cP ++ [cL]
+backjump s@(S _ ls (C _ cP cL _) _) = trace ("Backjumping: c = " ++ show c ++ " l = " ++ show cL ++ " level = " ++ show (getBackJumpLevel s)) $
+                                      setConflictReason (assertLiteral s' (-cL) False) (-cL) c
   where ls' = prefixToLevel ls $ getBackJumpLevel s
         s'  = s { litTrail = ls', conflict = C M.empty [] 0 0 }
+        c   = cP ++ [negate cL]
 
 getBackJumpLevel :: State -> Int
 getBackJumpLevel (S _ _  (C _ [] _ _) _) = 0
@@ -186,7 +188,7 @@ assertLiteral state l d = state { litTrail = litTrail state ++ [(l, d)] }
 
 decide :: State -> State
 decide s@(S f ls _ _) = trace ("Decided " ++ show (head unassignedVars)) $ assertLiteral s (head unassignedVars) True
-  where unassignedVars = fst . L.partition (not . (`elem` currentLits) . abs) . vars $ f
+  where unassignedVars = fst . L.partition (not . (`elem` currentLits)) . vars $ f
         currentLits    = map (abs . fst) ls
 
 backtrack :: State -> State
@@ -195,23 +197,32 @@ backtrack state = assertLiteral (state { litTrail = ls' }) (-l) False
         ls  = litTrail state
         ls' = prefixBeforeLastDecision ls
 
-unitPropogate :: State -> State
-unitPropogate s@(S f ls _ _) = foldl (\s' (u, r) -> trace ("Set " ++ show u ++ " with reason " ++ show r) $ setConflictReason (assertLiteral s' u False) u r) s unitClauses
+exhaustiveUnitPropogate :: State -> State
+exhaustiveUnitPropogate s = let (s'@(S f ls _ _), b) = unitPropogate s in if contradicts ls f || not b
+    then s'
+    else exhaustiveUnitPropogate s'
+
+unitPropogate :: State -> (State, Bool)
+unitPropogate s@(S f ls _ _)   = case unitClauses of
+    []       -> (s, False)
+    (u, r):_ -> trace ("UP: " ++ show u ++ " with reason " ++ show r) (setConflictReason (assertLiteral s u False) u r, True)
   where unitClauses            = map (first head) $ filter ((== 1) . length . fst) removeFalsifiedLits
         removeFalsifiedLits    = map (first (filter (not . (`elem` ls') . negate))) $ zip removeSatisfiedClauses removeSatisfiedClauses
         removeSatisfiedClauses = filter (not . any (`elem` ls')) f
         ls' = map fst ls
 
 solve :: State -> (LiteralTrail, Sat)
-solve s = let s1@(S f ls _ _) = unitPropogate s in
+solve s = let s1@(S f ls _ _) = exhaustiveUnitPropogate s in
     if contradicts ls f
         then let s2@(S _ ls' _ _) = applyConflict s1 in
              if currentLevel ls' == 0
                 then traceShow (learn (explainEmpty s2)) ([], UNSAT)
                 else solve $ backjump (learn (explainUIP s2))
-        else if satisfies ls f
+        else if length ls == length (vars f)
                 then (ls, SAT)
                 else solve $ decide s1
 
 main :: IO ()
-main = print . solve . (\f -> S f [] (C M.empty [] 0 0) M.empty) $ [[-1,2], [-3,4], [-1,-3,5], [-2,-4,-5], [-2,3,5,-6], [-1,3,-5,-6], [1,-6], [1,7]]
+main = print . solve . (\f -> S f m (C M.empty [] 0 0) r) $ [[-1,2], [-3,4], [-1,-3,5], [-2,-4,-5], [-2,3,5,-6], [-1,3,-5,-6], [1,-6], [1,7]]
+  where m = [(6, True), (1, False), (2, False), (7, True), (3, True)]
+        r = M.fromList [ (1, [1,-6]), (2, [-1,2])]
