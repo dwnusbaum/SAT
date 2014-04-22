0dce427fa9a021694f51f43fe52271f9dedea516
Devin Nusbaum <d.w.nusbaum@gmail.com>
2014-04-21 21:59:59 -0400

    Improved data structures for finding xor clauses

--- XorMatrix.hs
+++ XorMatrix.hs
@@ -41,57 +41,59 @@ rref :: Matrix -> Matrix
 rref = undefined
 
 data XorSearchRecord = SR
-   { toSearch :: [Clause]
-   , searched :: [Clause]
-   , inSearch :: Int
-   , found    :: [XorEquation]
+   { toSearch  :: [Clause]
+   , searched  :: [Clause]
+   , inSearch  :: S.Set (S.Set Literal)
+   , foundXors :: [XorEquation]
    }
    deriving (Show)
 
 resetSearchRecord :: XorSearchRecord -> XorSearchRecord
-resetSearchRecord (SR ts sd _ f) = SR (sd ++ ts) [] 0 f
+resetSearchRecord (SR ts sd is f) = SR (is' ++ sd ++ ts) [] S.empty f
+  where is' = S.toList . S.map S.toList $ is
+
+addXorClause :: XorSearchRecord -> SearchClause -> XorSearchRecord
+addXorClause (SR ts sd _ fxs) sc = SR (sd ++ ts) [] S.empty $ toXorEq sc : fxs
 
 data SearchClause = SC
    { vars         :: S.Set Literal
-   , foundClauses :: S.Set (S.Set Literal)
    , isPosSign    :: Bool
    }
 
 toXorEq :: SearchClause -> XorEquation
-toXorEq (SC v _ s) = XEq (S.toList v) s
+toXorEq (SC v s) = XEq (S.toList v) s
 
 findEquivalenceClause :: XorSearchRecord -> SearchClause -> XorSearchRecord
-findEquivalenceClause sr@(SR ts sd is fs) sc@(SC vs fcs _)
-  | is     == varsInSc   = resetSearchRecord sr { found = toXorEq sc : fs }
-  | is + 1 == varsInSc   = findLastClause (resetSearchRecord sr) sc
-  | null ts              = resetSearchRecord sr
-  | matches sc (head ts) = traceShow srMatch $ findEquivalenceClause srMatch scMatch
-  | otherwise            = findEquivalenceClause srNoMatch sc
+findEquivalenceClause sr@(SR ts sd is fs) sc@(SC vs _)
+  | S.size is     == varsInSc = addXorClause sr sc
+  | S.size is + 1 == varsInSc = findLastClause (SR (sd ++ ts) [] is fs) sc
+  | null ts                   = resetSearchRecord sr
+  | matches sc is (head ts)   = traceShow srMatch $ findEquivalenceClause srMatch sc
+  | otherwise                 = findEquivalenceClause srNoMatch sc
   where size      = S.size vs
         varsInSc  = 2 ^ (size - 1)
-        scMatch   = sc { foundClauses = S.insert (S.fromList (head ts)) fcs }
-        srMatch   = sr { toSearch = tail ts, inSearch = is + 1 }
+        srMatch   = sr { toSearch = tail ts, inSearch = S.insert (S.fromList (head ts)) is }
         srNoMatch = sr { toSearch = tail ts, searched = head ts : sd }
 
 findLastClause :: XorSearchRecord -> SearchClause -> XorSearchRecord
 findLastClause sr@(SR [    ] _  _  _) _ = resetSearchRecord sr
-findLastClause    (SR (t:ts) sd _ fs) sc@(SC vs _ _)
+findLastClause    (SR (t:ts) sd is fs) sc@(SC vs _)
   | S.size vsNotInT < S.size vs = if existsMatch
-                                      then SR (sd ++ ts) [] 0 (toXorEq sc : fs)
-                                      else findLastClause (SR ts (t : sd) 0 fs) sc
-  | otherwise = findLastClause (SR ts (t : sd) 0 fs) sc
+                                      then SR (sd ++ ts) [] S.empty (toXorEq sc : fs)
+                                      else findLastClause (SR ts (t : sd) is fs) sc
+  | otherwise = findLastClause (SR ts (t : sd) is fs) sc
   where vsNotInT    = vs S.\\ S.fromList (map abs t)
         tsInV       = S.filter (flip S.member vs . abs) (S.fromList t)
-        existsMatch = any (matches sc) $ testClauses (S.toList tsInV) (S.toList vsNotInT)
+        existsMatch = any (matches sc is) $ testClauses (S.toList tsInV) (S.toList vsNotInT)
         testClauses :: [Literal] -> [Literal] -> [Clause]
         testClauses xs [    ] = [xs]
         testClauses xs (y:ys) = testClauses (xs ++ [y]) ys ++ testClauses (xs ++ [-y]) ys
 
-matches :: SearchClause -> Clause -> Bool
-matches sc c = signsMatch && varsMatch && not usedAlready
-  where signsMatch  = isPositiveSign c       == isPosSign sc
-        varsMatch   = S.fromList (map abs c) == vars sc
-        usedAlready = F.any (== S.fromList c) $ foundClauses sc
+matches :: SearchClause -> S.Set (S.Set Literal) -> Clause -> Bool
+matches sc usedClauses newClause = signsMatch && varsMatch && not usedAlready
+  where signsMatch  = isPosSign sc == isPositiveSign newClause
+        varsMatch   = vars      sc == S.fromList (map abs newClause)
+        usedAlready = F.any (== S.fromList newClause) usedClauses
 
 isPositiveSign :: Clause -> Bool
 isPositiveSign c = odd $ length $ filter (>0) c
@@ -104,8 +106,8 @@ standardizeXEq (XEq c b) = uncurry XEq $ foldl work ([], b) c
 
 main :: IO ()
 main = print $ findEquivalenceClause sr sc
-  where sr = SR [[1,2], [1, -2, 3], [1, 2, -3]] [] 1 []
-        sc = SC (S.fromList [1, 2, 3]) (S.singleton (S.fromList [-1, 2, 3])) False
+  where sr = SR [[-1,-2,-3], [1, -2, 3], [1, 2, -3]] [] (S.singleton (S.fromList [-1,2,3])) []
+        sc = SC (S.fromList [1, 2, 3]) False
 
 {-
 
