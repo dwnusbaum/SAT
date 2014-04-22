478c74c4272e933f6770a90d00fde50a1ea67442
Devin Nusbaum <d.w.nusbaum@gmail.com>
2014-04-21 20:55:07 -0400

    Working on XOR clause recovery from the CNF

--- XorMatrix.hs
+++ XorMatrix.hs
@@ -3,6 +3,7 @@
 module XorMatrix where
 
 --import Control.Arrow (first, second)
+--import Control.Monad (replicateM)
 import Debug.Trace
 
 import qualified Data.Foldable as F
@@ -48,7 +49,7 @@ data XorSearchRecord = SR
    deriving (Show)
 
 resetSearchRecord :: XorSearchRecord -> XorSearchRecord
-resetSearchRecord (S ts sd _ f) = S (sd ++ ts) [] 0 f
+resetSearchRecord (SR ts sd _ f) = SR (sd ++ ts) [] 0 f
 
 data SearchClause = SC
    { vars         :: S.Set Literal
@@ -57,34 +58,40 @@ data SearchClause = SC
    }
 
 toXorEq :: SearchClause -> XorEquation
-toXorEq (Sch v _ s) = XEq (S.toList v) s
+toXorEq (SC v _ s) = XEq (S.toList v) s
 
 findEquivalenceClause :: XorSearchRecord -> SearchClause -> XorSearchRecord
-findEquivalenceClause sr@(SR ts sd is fs) sc@(SC vs fcs sgn)
+findEquivalenceClause sr@(SR ts sd is fs) sc@(SC vs fcs _)
   | is     == varsInSc   = resetSearchRecord sr { found = toXorEq sc : fs }
-  | is + 1 == varsInSc   = findLastClause srReset sc
+  | is + 1 == varsInSc   = findLastClause (resetSearchRecord sr) sc
   | null ts              = resetSearchRecord sr
   | matches sc (head ts) = traceShow srMatch $ findEquivalenceClause srMatch scMatch
   | otherwise            = findEquivalenceClause srNoMatch sc
   where size      = S.size vs
         varsInSc  = 2 ^ (size - 1)
-        scMatch   = sc  { foundClauses = S.insert (S.fromList (head ts)) fcs }
+        scMatch   = sc { foundClauses = S.insert (S.fromList (head ts)) fcs }
         srMatch   = sr { toSearch = tail ts, inSearch = is + 1 }
         srNoMatch = sr { toSearch = tail ts, searched = head ts : sd }
 
 findLastClause :: XorSearchRecord -> SearchClause -> XorSearchRecord
-findLastClause sr@(SR [    ] _  _  _) _ = resetSearchRecord s
-findLastClause    (SR (t:ts) sd _ fs) sc@(SC vs fcs sgn)
-  | S.Size vsNotInC < size vs =
-  | otherwise = findLastClause (SR ts (t:sd) 0 fs) sc
-  where vsNotInC = vs S.\\ (S.fromList (map abs c) --All of the vars we would need to add
-        subsets  = S.filter (S.isProperSubsetOf c) fcs
+findLastClause sr@(SR [    ] _  _  _) _ = resetSearchRecord sr
+findLastClause    (SR (t:ts) sd _ fs) sc@(SC vs _ _)
+  | S.size vsNotInT < S.size vs = if existsMatch
+                                      then SR (sd ++ ts) [] 0 (toXorEq sc : fs)
+                                      else findLastClause (SR ts (t : sd) 0 fs) sc
+  | otherwise = findLastClause (SR ts (t : sd) 0 fs) sc
+  where vsNotInT    = vs S.\\ S.fromList (map abs t)
+        tsInV       = S.filter (flip S.member vs . abs) (S.fromList t)
+        existsMatch = any (matches sc) $ testClauses (S.toList tsInV) (S.toList vsNotInT)
+        testClauses :: [Literal] -> [Literal] -> [Clause]
+        testClauses xs [    ] = [xs]
+        testClauses xs (y:ys) = testClauses (xs ++ [y]) ys ++ testClauses (xs ++ [-y]) ys
 
 matches :: SearchClause -> Clause -> Bool
 matches sc c = signsMatch && varsMatch && not usedAlready
   where signsMatch  = isPositiveSign c       == isPosSign sc
         varsMatch   = S.fromList (map abs c) == vars sc
-        usedAlready = S.any (== c) $ foundClauses sc
+        usedAlready = F.any (== S.fromList c) $ foundClauses sc
 
 isPositiveSign :: Clause -> Bool
 isPositiveSign c = odd $ length $ filter (>0) c
@@ -97,8 +104,8 @@ standardizeXEq (XEq c b) = uncurry XEq $ foldl work ([], b) c
 
 main :: IO ()
 main = print $ findEquivalenceClause sr sc
-  where sr = SRec [[-1, 2, 3], [1, -2, 3], [1, 2, -3], [-1, -2]] [] 1 []
-        sc = Sch (S.fromList [-1, -2, -3]) (S.singleton (S.fromList [-1,-2,-3])) False
+  where sr = SR [[1,2], [1, -2, 3], [1, 2, -3]] [] 1 []
+        sc = SC (S.fromList [1, 2, 3]) (S.singleton (S.fromList [-1, 2, 3])) False
 
 {-
 
