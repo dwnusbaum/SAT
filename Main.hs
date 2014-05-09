0003c0540d77eea08605d3d9e07a390dded2cafa
Devin Nusbaum <d.w.nusbaum@gmail.com>
2014-05-09 15:24:47 -0400

    Updated executable to take .cnf file as argument

--- Main.hs
+++ Main.hs
@@ -2,12 +2,20 @@
 
 module Main(main) where
 
+import System.Environment (getArgs)
+import System.Exit        (ExitCode(..), exitFailure, exitSuccess, exitWith)
+
 import ParseDimacs
 import SAT
+import Types
 
 main :: IO ()
 main = do
-    cnf <- parseDimacsFile "/Users/devin/Downloads/sat/sat-2002-beta/submitted/aloul/Bart/bart10.shuffled.cnf"
+    [cnfPath] <- getArgs
+    cnf <- parseDimacsFile cnfPath
     case cnf of
-        Left err -> print err
-        Right val -> print . solveFormula $ val
+        Left err -> print err >> exitWith (ExitFailure 65) --Input file was incorrectly formatted
+        Right val -> case solveFormula val of
+                         (_, UNSAT) -> putStrLn "Unsatisfiable"   >> exitFailure
+                         (_,   SAT) -> putStrLn "Satisfiable"     >> exitSuccess
+                         (_, UNDEF) -> putStrLn "Undefined error" >> exitFailure
